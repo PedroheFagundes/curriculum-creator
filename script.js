9da9879896439c6e4351dfd593d66dcc673e5e31
Pedro Fagundes <pedrohefagundes@gmail.com>
2021-09-01 08:47:09 -0300

    fix: code cleaning

--- script.js
+++ script.js
@@ -19,68 +19,48 @@ function selectSection() {
       console.log(sectionCounter);
       document.getElementById('personalInfo').setAttribute("class", "sectionOn");
       document.getElementById('previousButton').setAttribute("disabled", "disabled");
-      document.getElementById('previousButton').innerHTML = 'Previous';
-      document.getElementById('nextButton').innerHTML = 'Next: Expertise Area';
 
       break;
 
     case 1:
       document.getElementById('expertiseArea').setAttribute("class", "sectionOn");
       document.getElementById('previousButton').removeAttribute("disabled");
-      document.getElementById('previousButton').innerHTML = 'Previous: Personal Info';
-      document.getElementById('nextButton').innerHTML = 'Next: Professional Experience';
       break;
 
     case 2:
       document.getElementById('professionalExperience').setAttribute("class", "sectionOn");
-      document.getElementById('previousButton').innerHTML = 'Previous: Expertise Area';
-      document.getElementById('nextButton').innerHTML = 'Next: Certificates';
       break;
 
     case 3:
       document.getElementById('certificate').setAttribute("class", "sectionOn");
-      document.getElementById('previousButton').innerHTML = 'Previous: Professional Experience';
-      document.getElementById('nextButton').innerHTML = 'Next: Awards';
       break;
 
     case 4:
       document.getElementById('awards').setAttribute("class", "sectionOn");
-      document.getElementById('previousButton').innerHTML = 'Previous: Certificates';
-      document.getElementById('nextButton').innerHTML = 'Next: Professional Organizations';
       break;
 
     case 5:
       document.getElementById('professionalOrganization').setAttribute("class", "sectionOn");
-      document.getElementById('previousButton').innerHTML = 'Previous: Awards';
-      document.getElementById('nextButton').innerHTML = 'Next: Conferences & Courses';
       break;
 
     case 6:
       document.getElementById('conferencesCourses').setAttribute("class", "sectionOn");
-      document.getElementById('previousButton').innerHTML = 'Previous: Professional Organizations';
-      document.getElementById('nextButton').innerHTML = 'Next: Education';
       break;
 
     case 7:
       document.getElementById('education').setAttribute("class", "sectionOn");
-      document.getElementById('previousButton').innerHTML = 'Previous: Conferences & Courses';
-      document.getElementById('nextButton').innerHTML = 'Next: Languages';
       break;
 
     case 8:
       document.getElementById('languages').setAttribute("class", "sectionOn");
-      document.getElementById('previousButton').innerHTML = 'Previous: Education';
       document.getElementById('nextButton').removeAttribute("disabled");
-      document.getElementById('nextButton').innerHTML = 'Next: Interests';
       document.getElementById('formatedCurriculumButton').setAttribute("class", "sectionOff");
       document.getElementById('formatedCurriculum').setAttribute("class", "sectionOff");
       break;
 
     case 9:
       document.getElementById('interests').setAttribute("class", "sectionOn");
-      document.getElementById('previousButton').innerHTML = 'Previous: Languages';
       document.getElementById('nextButton').setAttribute("disabled", "disabled");
-      document.getElementById('nextButton').innerHTML = 'Next';
       document.getElementById('formatedCurriculumButton').setAttribute("class", "sectionOn");
       break;
   }
